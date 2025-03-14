b9e90e792dfeb5f7a4981068ecfa66cf496c3f8c
ibrahim-shahin23 <ibrahimsaaed38@gmail.com>
2025-03-15 00:01:06 +0200

    feature(dashboard) query all entities from DB, display totals

--- Warehouse/dashboard/views.py
+++ Warehouse/dashboard/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
 
+from inventory.models import Category, Product
+from orders.models import Order
+from shipments.models import Shipment
+from users.models import User
+
 def get_dashboard_page(request):
-    return render(request, 'dashboard.html')
+    users = User.objects.all()
+    products = Product.objects.all()
+    categories = Category.objects.all()
+    orders = Order.objects.all()    
+    shipments = Shipment.objects.all()
+    return render(request, 'dashboard.html', {'users': users, 'products': products, 'categories': categories, 'orders': orders, 'shipments': shipments})
