c150e2b9585e5e7a4dbc77ef0bb0cb8dcbba7833
Maiabuelkheir <Maiabuelkheir27@gmail.com>
2025-03-21 00:03:26 +0200

    feature(inventory):search,filter and highlight stock

--- Warehouse/Warehouse/settings.py
+++ Warehouse/Warehouse/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'shipments',
     'orders',
     'dashboard',
+    
 ]
 
 MIDDLEWARE = [
--- Warehouse/inventory/models.py
+++ Warehouse/inventory/models.py
@@ -10,6 +10,11 @@ class Category(models.Model):
         return f"{self.name}"
 
 class Product(models.Model):
+    STOCK_CHOICES = [
+        ("in_stock", "In Stock"),
+        ("low_stock", "Low Stock"),
+        ("out_of_stock", "Out of Stock"),
+    ]
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     sku = models.CharField(max_length=50, unique=True)
@@ -17,13 +22,29 @@ class Product(models.Model):
     critical_quantity = models.PositiveIntegerField(default=10)
     category=models.ForeignKey(Category, on_delete=models.DO_NOTHING)
     thumbnail = models.URLField(null=True, blank=True)
+    status = models.CharField(max_length=20, choices=STOCK_CHOICES, default="in_stock")
+
 
     def __str__(self):
         return f"{self.name} ({self.sku})"
 
     def is_low_stock(self):
+      
         return self.quantity <= self.critical_quantity
 
     def is_out_of_stock(self):
+       
         return self.quantity == 0
 
+    def update_stock_status(self):
+        
+        if self.is_out_of_stock():
+            self.status = "out_of_stock"
+        elif self.is_low_stock():
+            self.status = "low_stock"
+        else:
+            self.status = "in_stock"
+
+    def save(self, *args, **kwargs):
+        self.update_stock_status()
+        super().save(*args, **kwargs)
