f2bb7a25e8a97f53339fd0b092b88ea013047704
AhmedFathalla7 <am777411@gmail.com>
2025-03-21 06:10:36 +0200

    feature(shipments):search,filter-stock-add,delete item

--- Warehouse/shipments/forms.py
+++ Warehouse/shipments/forms.py
@@ -1,11 +1,21 @@
 from django import forms
 from django.forms import inlineformset_factory
-from .models import Shipment, ShipmentLineItem
+from .models import Shipment, ShipmentLineItem ,Factory
 
 class ShipmentForm(forms.ModelForm):
+    factory = forms.ModelChoiceField(
+        queryset=Factory.objects.all(),
+        widget=forms.Select(attrs={'class': 'form-control'}),
+        empty_label="-- Select Factory --"
+    )
     class Meta:
         model = Shipment
         fields = ['reference', 'factory', 'status']
+        widgets = {
+            'reference': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter reference'}),
+            'status': forms.Select(attrs={'class': 'form-control'}),
+        }
+
 
 class ShipmentLineItemForm(forms.ModelForm):
     class Meta:
@@ -15,7 +25,9 @@ class ShipmentLineItemForm(forms.ModelForm):
 ShipmentLineItemFormSet = inlineformset_factory(
     Shipment, ShipmentLineItem,
     form=ShipmentLineItemForm,
-    extra=0,
+    extra=1,
     can_delete=False
 )
+
+
 
